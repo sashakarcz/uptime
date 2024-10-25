d83b53ba15091cab8ccef7631ea9279b30e19748
Sasha Karcz <sasha@starnix.net>
2024-10-25 10:22:06 -0500

    Update dns_check.py

--- python/dns_check.py
+++ python/dns_check.py
@@ -89,7 +89,9 @@ for entry in config['sites']:
 
         # Run the DNS check
         result = run_dns_check(domain, expected_record, record_type, url)
+        readme_file.seek(0)
         results.append(result)
+        readme_file.truncate()
 
 # Write the results to a YAML file
 with open('history/dns_results.yml', 'w') as outfile:
